c6c40f5db4cc134996b9a989728ecd420de8d749
Erick Guzmán <guzmi.dev@gmail.com>
2022-04-08 02:53:29 -0600

    :tada: disenos-flutter-practice

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Material App',
+        home: _TempHomeScreen());
+  }
+}
+
+class _TempHomeScreen extends StatelessWidget {
+  const _TempHomeScreen({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: const [
+        Text('Hola Mundo'),
+        Text('Hola Mundo'),
+      ],
+    );
+  }
+}
